331b5f7f02b342b49e5126df4e36de5f5715d164
Vinicius Brito <brito.vini@Outlook.com>
2026-05-23 20:36:14 -0300

    Trabalhando com colunas

---
+++ DIa02/05.colunas.py
@@ -0,0 +1,37 @@
+#%%
+
+import pandas as pd
+
+df = pd.read_csv("../dados/transacoes.csv", sep=";")
+#%%
+df.shape
+# %%
+df.dtypes
+# %%
+## renomear coluna ###
+## esse tipo de rename precisa atribuir ao df;
+
+renamed_columns = {
+        "QtdePontos" : "qtPontos",
+        "DescSistemaOrigem": "SistemaOrigem"
+    }
+#df = df.rename(columns=renamed_columns)
+df.rename(columns=renamed_columns, inplace=True) #-> Altera sem precisar reatribuir
+
+# %%
+df
+# %%
+## Pegar colunas ##
+# So pode passar um unico objeto pra dentro do dataframe, então passa uma lista que é um unico objeto carrega varios elementos
+
+df[["IdCliente",'qtPontos']]
+# %%
+df[["IdCliente",'qtPontos']].head(5)
+# %%
+colunas = df.columns.to_list()
+colunas.sort()
+colunas
+# %%
+df = df[colunas]
+df
+# %%
